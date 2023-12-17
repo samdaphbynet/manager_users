33d550c1fe052693cd4f51d7191fef57a62adcff
samdaphbynet <lj8@hotmail.fr>
2023-12-17 20:29:36 +0100

    fixed cros

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const path = require("path")
 
 const app = express();
 app.use(cors({
-    origin: ["https://manager-users-client.vercel.app/"],
+    origin: ["http://localhost:5173"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
 }));
